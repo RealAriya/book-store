a413b59c00d1158de4c4ba4e33da9f0475468a21
Arya <arya.t.innovate@gmail.com>
2025-02-09 18:32:28 +0330

    feat : add delete method to database class
    + It's delete records by getting id

--- Query.py
+++ Query.py
@@ -24,3 +24,8 @@ class Database:
         self.cur.execute("FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
         rows = self.cur.fetchall()
         return rows
+    
+
+    def delete(self, id):
+        self.cur.execute("DELETE FROM book WHERE id=?", (id,))
+        self.conn.commit()
