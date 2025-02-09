7b5932f653e66b2a8a043f7ce74f6b67c772dff1
Arya <arya.t.innovate@gmail.com>
2025-02-09 14:09:33 +0330

    feat: add search_command method for searching in the database
    - The search_command
      method allows users to
      search the database using
      title, author, year, and ISBN
      inputs. Results are displayed
      in the list1 widget.

--- script.py
+++ script.py
@@ -126,6 +126,13 @@ class Root(Tk):
         for row in database.view():
             # Add new rows at the end.
             self.list1.insert(END, row)
+
+    
+    def search_command(self):
+        self.list1.delete(0,END)
+        # uses get for a search method
+        for row in database.search(self.title_text.get(), self.author_text.get(), self.year_text.get(), self.isbn_text.get()):
+            self.list1.insert(END, row)
             
 
 
